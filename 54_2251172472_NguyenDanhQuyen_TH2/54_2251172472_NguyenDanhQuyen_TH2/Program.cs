20b9193beae026b54f7e2bf3aacabccecd4d87bd
quyendanh04 <quyendanh04@gmail.com>
2025-05-13 17:49:28 +0700

    add th2

---
+++ 54_2251172472_NguyenDanhQuyen_TH2/54_2251172472_NguyenDanhQuyen_TH2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    const int n = 10;
+    static Queue<int> A = new Queue<int>();
+
+    static Semaphore empty = new Semaphore(n, n);
+    static Semaphore full = new Semaphore(0, n);
+
+    static object bufferLock = new object();
+
+    static Random random = new Random();
+
+    static void Main()
+    {
+        int k = 3;
+        int h = 2;
+
+        for (int i = 1; i <= k; i++)
+        {
+            int id = i;
+            new Thread(() => SinhDuLieu(id)).Start();
+        }
+
+        for (int i = 1; i <= h; i++)
+        {
+            int id = i;
+            new Thread(() => XuLyDuLieu(id)).Start();
+        }
+    }
+
+    static void SinhDuLieu(int id)
+    {
+        while (true)
+        {
+            int value = random.Next(1000);
+
+            empty.WaitOne();
+
+            lock (bufferLock)
+            {
+                A.Enqueue(value);
+                Console.WriteLine($"P{id}: {value} - {DateTime.Now:HH:mm:ss.fff}");
+            }
+
+            full.Release();
+
+            Thread.Sleep(random.Next(300, 1000));
+        }
+    }
+
+    static void XuLyDuLieu(int id)
+    {
+        while (true)
+        {
+            full.WaitOne();
+
+            int value;
+            lock (bufferLock)
+            {
+                value = A.Dequeue();
+            }
+
+            empty.Release();
+
+            int result = value * 2;
+            Console.WriteLine($"C{id}: {value} - {result} - {DateTime.Now:HH:mm:ss.fff}");
+
+            Thread.Sleep(random.Next(500, 1200));
+        }
+    }
+}
